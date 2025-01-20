2514a2e9a9767b0be44e7b3ced59d2e670518bc1
Ирина Кайратовна <irina@exmple.com>
2025-01-20 20:29:00 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+// let first_num = document.querySelector('#first')
+// let second_num = document.querySelector('#second')
+// let math = document.querySelector('#math')
+// let button = document.querySelector('#bth')
+// let resultt = document.querySelector('#result')
+
+// function calculate(){
+//     let fr = first_num.get
+//     let sc = second_num.get
+//     let mth = math.get
+
+//     let result = 0
+
+//     if(mth == '+'){
+//         result = fr + sc
+//     }else if(mth == '-'){
+//         result = fr - sc
+//     }else if(mth == '*'){
+//         result = fr * sc
+//     }else if(mth == '/'){
+//         result = fr / cs
+//     }else if(mth == '**'){
+//         result = fr ** cs
+//     }else{
+//         result = 'error'
+//     }
+//     resultt = result
+// }
+
+// button.addEventListener('click', calculate)
+
+let fruits = ['apple', 'orange', 'pineapple']
+
+let i = -1
+
+while(i < fruits.length){
+    i++
+    console.log(fruits[i])
+}
+
